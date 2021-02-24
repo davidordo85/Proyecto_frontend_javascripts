88c8c8341a92098c60e5489b12a8a3ac02f54fb1
David Ordoñez <davidordonezescacena@hotmail.es>
2021-02-24 09:12:38 +0100

    Chapa y pintura

--- Proyecto_nodepop_javascripts/NodeBooks/js/views.js
+++ Proyecto_nodepop_javascripts/NodeBooks/js/views.js
@@ -1,8 +1,25 @@
+
 export const adsView = (advertisement) => {
-    return `<div class="post">
-    <strong class="name">${advertisement.name}</strong>
-    <i class="author">${advertisement.author}</i>
+    return `<div class="card">
+    <div class="card-content">
+    <div class="media">
+
+    <div class="media-content">
+    <p class="title is-4">${advertisement.name}</p>
+    <i class="subtitle is-6">${advertisement.author}</i>
+    </div>
+    </div>
+    
+    <div class="content">
     <p class="price">${advertisement.price}</p>
     <p class="sale">${advertisement.sale}</p>
-</div>`;
+    </div>
+    </div>
+    <br>
+    <div class="card-image">
+      <figure class="image is-4by3">
+        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+      </figure>
+    </div>
+    </div>`;
 }
