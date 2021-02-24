cd23c2671958a4904ddf546dd178ba67dc81b184
David Ordoñez <davidordonezescacena@hotmail.es>
2021-02-24 13:52:08 +0100

    Al registrarse te manda al login

--- Proyecto_nodepop_javascripts/NodeBooks/js/controller/RegisterFormController.js
+++ Proyecto_nodepop_javascripts/NodeBooks/js/controller/RegisterFormController.js
@@ -21,6 +21,7 @@ export default class RegisterFormController extends BaseController {
             try {
                 const data = await dataService.registerUser(user);
                 alert('Usuario creado con éxito!')
+                window.location.href = '/login.html';
             } catch (error) {
                 this.publish(this.events.ERROR, error);
             } finally {
---
+++ Proyecto_nodepop_javascripts/NodeBooks/js/login.js
@@ -0,0 +1,10 @@
+import LoaderController from './controller/LoaderController.js';
+import ErrorController from './controller/ErrorController.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const loader = document.querySelector('lds-ellipsis');
+    new LoaderController(loader);
+
+    const errorsElement = document.querySelector('.global-errors');
+    new ErrorController(errorsElement);
+})
